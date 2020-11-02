0fc855b62d2fe2be8911bca910630102b17f2650
Daniel Litt <daniel_litt@comcast.net>
2020-11-02 15:40:18 -0500

    Add post id to vote count class

--- vote-buttons.php
+++ vote-buttons.php
@@ -5,9 +5,10 @@ $votes = ($votes == "") ? 0 : $votes;
 
 // Linking to the admin-ajax.php file. Nonce check included for extra security. Note the "user_like" class for JS enabled clients.
 $nonce = wp_create_nonce("user_vote_nonce");
-$link = admin_url('admin-ajax.php?action=user_vote&post_id='.get_the_ID().'&nonce='.$nonce);
+$post_id = get_the_ID();
+$link = admin_url('admin-ajax.php?action=user_vote&post_id='.$post_id.'&vote_direction=1' .'&nonce='.$nonce);
 echo "<div style=\"text-align:center;\">";
-echo '<a class="user_like" data-nonce="' . $nonce . '" data-post_id="' . get_the_ID() . '" href="' . $link . '">Upvote</a><br>';
-echo "<span id=\"vote_counter\">$votes</span><br>";
-echo '<a class="user_like" data-nonce="' . $nonce . '" data-post_id="' . get_the_ID() . '" href="' . $link . '">Downvote</a>';
+echo '<a class="user_vote" data-nonce="' . $nonce . '" data-post_id="' . $post_id . '" href="' . $link . '">Upvote</a><br>';
+echo "<span id='vote_counter_$post_id'>$votes</span><br>";
+echo '<a class="user_vote" data-nonce="' . $nonce . '" data-post_id="' . $post_id . '" href="' . $link . '">Downvote</a>';
 echo "</div>";
--- vote_display.js
+++ vote_display.js
@@ -1,5 +1,5 @@
 jQuery(document).ready( function() {
-    jQuery(".user_like").click( function(e) {
+    jQuery(".user_vote").click( function(e) {
         e.preventDefault();
         post_id = jQuery(this).attr("data-post_id");
         nonce = jQuery(this).attr("data-nonce");
@@ -10,7 +10,7 @@ jQuery(document).ready( function() {
             data : {action: "user_vote", post_id : post_id, nonce: nonce},
             success: function(response) {
                 if(response.type === "success") {
-                    jQuery("#vote_counter").html(response.vote_count);
+                    jQuery("#vote_counter_"+post_id).html(response.vote_count);
                 }
                 else {
                     alert("Your like could not be added");
